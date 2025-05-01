cc56f248ea5e06b060b198aa1af9d28d8aa1eb5a
Zyn <davilucastokmuller@gmail.com>
2025-05-01 09:46:31 -0300

    Update commands.js

--- src/bot/commands.js
+++ src/bot/commands.js
@@ -1,1 +1,25 @@
+const kaomojis = require('./kaomoji.json');
 
+exports.handleKaomojiCommand = (command) => {
+  const args = command.split(' ');
+  const action = args[1]; // Ex: "find", "add", "list"
+
+  switch (action) {
+    case 'list':
+      return `Categorias disponíveis: ${Object.keys(kaomojis).join(', ')}`;
+    case 'find':
+      const keyword = args[2];
+      return findKaomojiByKeyword(keyword);
+    default:
+      return getRandomKaomoji(action || 'all');
+  }
+};
+
+function getRandomKaomoji(category) {
+  const list = kaomojis[category] || Object.values(kaomojis).flat();
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+function findKaomojiByKeyword(keyword) {
+  // Implemente busca por tags (ex: "table" → ┻━┻)
+}
