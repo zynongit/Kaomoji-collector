c448f908810a681e78fcd92bf97f6dcfc9ffcef3
Zyn <davilucastokmuller@gmail.com>
2025-05-01 09:46:13 -0300

    Update index.js

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,1 +1,17 @@
+const { Probot } = require('probot');
+const commands = require('./commands');
 
+module.exports = (app) => {
+  app.on('issue_comment.created', async (context) => {
+    const comment = context.payload.comment.body;
+    if (comment.startsWith('/kaomoji')) {
+      const response = await commands.handleKaomojiCommand(comment);
+      await context.octokit.issues.createComment({
+        issue_number: context.issue().number,
+        owner: context.repo().owner,
+        repo: context.repo().repo,
+        body: response,
+      });
+    }
+  });
+};
